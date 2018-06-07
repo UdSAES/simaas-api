031525be1506ea4390e9bd08eca2834b89cbeb08
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2018-06-07 15:01:47 +0200

    Do NOT use proxy for worker-process

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 
 # Start all sub-services
 ( cd /home/node/queue; LISTEN_PORT=$LISTEN_PORT_QUEUE node index.js ) &
-( cd /home/node/worker; node index.js ) &
+( cd /home/node/worker; unset HTTP_PROXY; unset HTTPS_PROXY; unset FTP_PROXY; unset NO_PROXY; unset http_proxy; unset https_proxy; unset ftp_proxy; unset no_proxy; node index.js ) &
 ( cd /home/node/api; node index.js )
