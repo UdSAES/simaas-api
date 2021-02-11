ae269e1a65f86a85cb3b4e38da9956c27f065d1a
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-02-11 18:30:34 +0100

    Store config in one global object instead of several ones

--- index.js
+++ index.js
@@ -35,41 +35,43 @@ const responseUtils = require('./lib/response_utils.js')
 
 log.info({ code: 300010 }, 'successfully loaded modules')
 
-// Load configuration
-const QUEUE_ORIGIN = process.env.QUEUE_ORIGIN
-const LISTEN_PORT = parseInt(process.env.LISTEN_PORT)
-const UI_STATIC_FILES_PATH = String(process.env.UI_STATIC_FILES_PATH) || ''
-const UI_URL_PATH = String(process.env.UI_URL_PATH) || ''
-const ALIVE_EVENT_WAIT_TIME = parseInt(process.env.ALIVE_EVENT_WAIT_TIME) || 3600 * 1000
-const API_SPECIFICATION_FILE_PATH = './oas/simaas_oas3_flat.yaml'
+// Declare global object for storing config; populated in `loadConfig`
+let cfg = {}
 
 // Define functions
 async function checkIfConfigIsValid () {
-  if (!_.isString(QUEUE_ORIGIN)) {
-    log.fatal(
-      { code: 600020 },
-      'QUEUE_ORIGIN is ' +
-        QUEUE_ORIGIN +
-        ' but must be a valid protocol+host-combination (e.g. http://127.0.0.1:12345)'
-    )
-    process.exit(1)
+  const config = {
+    listenPort: parseInt(process.env.LISTEN_PORT),
+    heartbeatFrequency: parseInt(process.env.ALIVE_EVENT_WAIT_TIME) || 3600 * 1000,
+    ui: {
+      staticFilesPath: String(process.env.UI_STATIC_FILES_PATH) || '',
+      urlPath: String(process.env.UI_URL_PATH) || ''
+    },
+    oasFilePath: './oas/simaas_oas3.json',
+    oasFilePathFlat: './oas/simaas_oas3_flat.yaml'
   }
 
-  if (!(_.isNumber(LISTEN_PORT) && LISTEN_PORT > 0 && LISTEN_PORT < 65535)) {
+  if (
+    !(
+      _.isNumber(config.listenPort) &&
+      config.listenPort > 0 &&
+      config.listenPort < 65535
+    )
+  ) {
     log.fatal(
       { code: 600020 },
       'LISTEN_PORT is ' +
-        LISTEN_PORT +
+        config.listenPort +
         ' but must be an integer number larger than 0 and smaller than 65535'
     )
     process.exit(1)
   }
 
-  if (!(_.isNumber(ALIVE_EVENT_WAIT_TIME) && ALIVE_EVENT_WAIT_TIME > 0)) {
+  if (!(_.isNumber(config.heartbeatFrequency) && config.heartbeatFrequency > 0)) {
     log.fatal(
       { code: 600020 },
       'ALIVE_EVENT_WAIT_TIME is ' +
-        ALIVE_EVENT_WAIT_TIME +
+        config.heartbeatFrequency +
         ' but must be positive integer number larger than 0'
     )
     process.exit(1)
@@ -82,11 +84,13 @@ async function checkIfConfigIsValid () {
   // TODO check validity of API_SPECIFICATION_FILE_PATH
 
   log.info({ code: 300020 }, 'configuration is valid, moving on')
+
+  return config
 }
 
 async function aliveLoop () {
   while (true) {
-    await delay(ALIVE_EVENT_WAIT_TIME)
+    await delay(cfg.heartbeatFrequency)
     log.info({ code: 300100 }, 'service instance still running')
   }
 }
@@ -108,7 +112,7 @@ function shutDownGracefully () {
 
 // Define main program
 async function init () {
-  await checkIfConfigIsValid()
+  cfg = await checkIfConfigIsValid()
 
   // Instantiate express-application and set up middleware-stack
   const app = express()
@@ -120,27 +124,27 @@ async function init () {
   let backend = null
   try {
     backend = new OpenAPIBackend({
-      definition: API_SPECIFICATION_FILE_PATH,
+      definition: cfg.oasFilePath,
       strict: true,
       validate: true,
       ajvOpts: {
         format: false
       }
     })
-    log.info('successfully loaded API description ' + API_SPECIFICATION_FILE_PATH)
+    log.info('successfully loaded API description ' + cfg.oasFilePath)
   } catch (error) {
-    log.fatal('error while loading API description ' + API_SPECIFICATION_FILE_PATH)
+    log.fatal('error while loading API description ' + cfg.oasFilePath)
     process.exit(1)
   }
 
   backend.init()
 
   // Expose UI iff UI_URL_PATH is not empty
-  if (UI_URL_PATH !== '') {
-    if (UI_STATIC_FILES_PATH !== '') {
+  if (cfg.ui.urlPath !== '') {
+    if (cfg.ui.staticFilesPath !== '') {
       // Expose locally defined UI
-      app.use(UI_URL_PATH, express.static(UI_STATIC_FILES_PATH))
-      log.info({ code: 300020 }, 'exposing UI as ' + UI_URL_PATH)
+      app.use(cfg.ui.urlPath, express.static(cfg.ui.staticFilesPath))
+      log.info({ code: 300020 }, 'exposing UI as ' + cfg.ui.urlPath)
     } else {
       // Fall back to default-UI
       log.fatal({ code: 600020 }, 'default-UI not implemented')
@@ -149,7 +153,7 @@ async function init () {
 
     // Redirect GET-request on origin to UI iff UI is exposed
     app.get('', async (req, res) => {
-      res.redirect(UI_URL_PATH)
+      res.redirect(cfg.ui.urlPath)
     })
   }
 
@@ -198,15 +202,15 @@ async function init () {
   log.info({ code: 300030 }, 'configuration successfull')
 
   // Start listening to incoming requests
-  server = app.listen(LISTEN_PORT, function () {
-    log.info({ code: 300040 }, 'now listening on port ' + LISTEN_PORT)
+  server = app.listen(cfg.listenPort, function () {
+    log.info({ code: 300040 }, 'now listening on port ' + cfg.listenPort)
   })
 
   // XXX is this even functional?
   app.on('error', function (error) {
     log.fatal(
       { code: 600030, err: error },
-      'cannot bind to listening port ' + LISTEN_PORT
+      'cannot bind to listening port ' + cfg.listenPort
     )
     process.exit(1)
   })
