340435bda9074e63d81ee80a617bff3ba4e22e1c
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-11-10 15:38:48 +0100

    Add handlers for exposing modeltypes, -variables, and -units

--- index.js
+++ index.js
@@ -178,6 +178,9 @@ async function init () {
   const backend = handlers.initializeBackend(cfg.oas.filePathDynamic)
 
   // Pass requests to middleware
+  app.get('/models/:id/types', handlers.getModelTypes)
+  app.get('/models/:id/units', handlers.getModelUnits)
+  app.get('/models/:id/variables', handlers.getModelVariables)
   app.use((req, res, next) => backend.handleRequest(req, req, res, next))
 
   // Serialize any remaining errors as JSON
